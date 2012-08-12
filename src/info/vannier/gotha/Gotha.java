c86aa6472e972621141a7c539bea4c0cd78fd47a
Luc <Luc@Asus>
2012-08-12 17:52:55 +0200

    V3.27.02

--- src/info/vannier/gotha/Gotha.java
+++ src/info/vannier/gotha/Gotha.java
@@ -36,8 +36,8 @@ public class Gotha {
 
     static Locale locale = Locale.getDefault();
     static final long GOTHA_VERSION = 327L;
-    static final long GOTHA_MINOR_VERSION = 2L;
-    static final java.util.Date GOTHA_RELEASE_DATE = (new GregorianCalendar(2012, Calendar.JUNE, 20)).getTime();
+    static final long GOTHA_MINOR_VERSION = 3L;
+    static final java.util.Date GOTHA_RELEASE_DATE = (new GregorianCalendar(2012, Calendar.AUGUST, 12)).getTime();
     // Data version for serialization
     static final long GOTHA_DATA_VERSION = 201L;
     // Should definitely stay below or equal to 32, due to internal limits in costValue() method
